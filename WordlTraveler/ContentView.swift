fd957172c4c34c851f1b4f18c7659e94a1ed570b
Hicret AY <hiicretay@hotmail.com>
2022-10-13 17:11:17 +0300

    converter added

--- WordlTraveler/ContentView.swift
+++ WordlTraveler/ContentView.swift
@@ -6,83 +6,53 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var fahrenheitValue: String = ""
+    
+    let numberFormatter: NumberFormatter = {
+       let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter
+    }()
+    
+    func converToCelcius() -> String {
+        
+        if let value = Double(fahrenheitValue){
+            let fahrenheit = Measurement<UnitTemperature>(
+                value: value,
+                unit: .fahrenheit)
+            
+            let celciusValue = fahrenheit.converted(to: .celsius)
+            
+            return numberFormatter.string(from: NSNumber(value: celciusValue.value)) ?? "?"
+            
+        }else{
+            return "?"
+        }
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        VStack{
+            TextField("value",text: $fahrenheitValue)
+                .font(Font.system(size: 64.0))
+                .multilineTextAlignment(.center)
+                .keyboardType(.decimalPad)
+            Text("fahrenheit")
+            Text("is actually").foregroundColor(.gray)
+            Text(converToCelcius())
+            Text("degress Celcius")
+            Spacer() // kalan alanı doldurur
+        }.foregroundColor(.orange)
+        .font(.title)
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
     }
 }
+
